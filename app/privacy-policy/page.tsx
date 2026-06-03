9b71326dab73cbecfbb10c913722b52802c34b48
Hanifa Anum <hanifaanum5@gmail.com>
2026-06-03 07:15:24 -0700

    Updated Static Pages

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { Metadata } from "next";
 import PrivacyHero from "@/components/PrivacyHero";
 import GetInTouch from "@/components/GetInTouch";
--- app/terms-and-conditions/page.tsx
+++ app/terms-and-conditions/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { Metadata } from "next";
 import TermsHero from "@/components/TermsHero";
 import GetInTouch from "@/components/GetInTouch";
